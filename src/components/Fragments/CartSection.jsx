5ee50570c1b34118cbbb2f8df0694dd5a91c98a9
Moch Farrel <91719525+mochfarrel06@users.noreply.github.com>
2023-12-24 11:50:21 +0700

    Create a cart section with product data and use a map

--- src/components/Fragments/CartSection.jsx
+++ src/components/Fragments/CartSection.jsx
@@ -1,5 +1,60 @@
 import {MinusIcon, PlusIcon, TrashIcon} from "@heroicons/react/20/solid";
 
+const products = [
+  {
+    id: 1,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: "$109.95",
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    rating: {
+      rate: 3.9,
+      count: 120,
+    },
+  },
+  {
+    id: 2,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: "$109.95",
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    rating: {
+      rate: 3.9,
+      count: 120,
+    },
+  },
+  {
+    id: 3,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: "$109.95",
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    rating: {
+      rate: 3.9,
+      count: 120,
+    },
+  },
+  {
+    id: 4,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: "$109.95",
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    rating: {
+      rate: 3.9,
+      count: 120,
+    },
+  },
+];
+
 export default function CartSection() {
   return (
     <div className="py-24 max-[400px]:py-20 sm:py-28 lg:py-28">
@@ -12,132 +67,54 @@ export default function CartSection() {
             {/* Product cart */}
             <div className="divide-y divide-gray-200 mb-10">
               {/* Product 1 */}
-              <div className="flex gap-5 items-center px-2 py-4 max-[400px]:items-start lg:gap-10">
-                <div className="max-[400px]:mt-10 mt-0">
-                  <input
-                    type="checkbox"
-                    className="w-5 h-5 border-gray-500 max-[400px]:w-4 max-[400px]:h-4"
-                  />
-                </div>
-                <div className="flex gap-5 w-full overflow-hidden justify-between max-[400px]:gap-3 max-[400px]:flex-col max-[400px]:items-center max-[400px]:justify-center lg:gap-8 lg:max-w-md">
-                  <div className="h-full w-16 flex-shrink-0 overflow-hidden max-[400px]:w-28 lg:w-24">
-                    <img
-                      src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-                      className="w-full h-full object-cover object-center"
+              {products.map((item) => (
+                <div
+                  key={item.id}
+                  className="flex gap-5 items-center px-2 py-4 max-[400px]:items-start lg:gap-10"
+                >
+                  <div className="max-[400px]:mt-10 mt-0">
+                    <input
+                      type="checkbox"
+                      className="w-5 h-5 border-gray-500 max-[400px]:w-4 max-[400px]:h-4"
                     />
                   </div>
-                  <div className="flex flex-col gap-4 overflow-hidden w-full">
-                    <h4 className="text-sm font-medium text-gray-700 truncate max-[400px]:text-xs lg:text-base">
-                      Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
-                    </h4>
-                    <div className="flex flex-col gap-4">
-                      <h3 className="text-base font-medium text-gray-800 max-[400px]:text-sm lg:text-lg">
-                        $109.95
-                      </h3>
-                      <div className="flex items-center gap-5 max-[400px]:gap-3">
-                        <button className="py-2">
-                          <TrashIcon className="w-5 h-5 text-gray-500 max-[400px]:w-4 max-[400px]:h-4" />
-                        </button>
-                        <div className="flex gap-4 max-[400px]:gap-2 items-center">
-                          <button className="bg-gray-700 w-8 h-8 px-2 py-2 flex items-center justify-center rounded-md max-[400px]:py-1">
-                            <MinusIcon className="w-4 h-4 text-white max-[400px]:w-3 max-[400px]:h-3" />
+                  <div className="flex gap-5 w-full overflow-hidden justify-between max-[400px]:gap-3 max-[400px]:flex-col max-[400px]:items-center max-[400px]:justify-center lg:gap-8 lg:max-w-md">
+                    <div className="h-full w-16 flex-shrink-0 overflow-hidden max-[400px]:w-28 lg:w-24">
+                      <img
+                        src={item.image}
+                        alt={item.title}
+                        className="w-full h-full object-cover object-center"
+                      />
+                    </div>
+                    <div className="flex flex-col gap-4 overflow-hidden w-full">
+                      <h4 className="text-sm font-medium text-gray-700 truncate max-[400px]:text-xs lg:text-base">
+                        {item.title}
+                      </h4>
+                      <div className="flex flex-col gap-4">
+                        <h3 className="text-base font-medium text-gray-800 max-[400px]:text-sm lg:text-lg">
+                          {item.price}
+                        </h3>
+                        <div className="flex items-center gap-5 max-[400px]:gap-3">
+                          <button className="py-2">
+                            <TrashIcon className="w-5 h-5 text-gray-500 max-[400px]:w-4 max-[400px]:h-4" />
                           </button>
-                          <span className="text-gray-800 text-base py-2 items-center font-medium max-[400px]:text-sm max-[400px]:py-1">
-                            1
-                          </span>
-                          <button className="bg-gray-700 w-8 h-8 px-2 py-2 flex items-center justify-center rounded-md max-[400px]:py-1">
-                            <PlusIcon className="w-4 h-4 text-white text-base max-[400px]:w-3 max-[400px]:h-3" />
-                          </button>
+                          <div className="flex gap-4 max-[400px]:gap-2 items-center">
+                            <button className="bg-gray-700 w-8 h-8 px-2 py-2 flex items-center justify-center rounded-md max-[400px]:py-1">
+                              <MinusIcon className="w-4 h-4 text-white max-[400px]:w-3 max-[400px]:h-3" />
+                            </button>
+                            <span className="text-gray-800 text-base py-2 items-center font-medium max-[400px]:text-sm max-[400px]:py-1">
+                              1
+                            </span>
+                            <button className="bg-gray-700 w-8 h-8 px-2 py-2 flex items-center justify-center rounded-md max-[400px]:py-1">
+                              <PlusIcon className="w-4 h-4 text-white text-base max-[400px]:w-3 max-[400px]:h-3" />
+                            </button>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="flex gap-5 items-center px-2 py-4 max-[400px]:items-start lg:gap-10">
-                <div className="max-[400px]:mt-10 mt-0">
-                  <input
-                    type="checkbox"
-                    className="w-5 h-5 border-gray-500 max-[400px]:w-4 max-[400px]:h-4"
-                  />
-                </div>
-                <div className="flex gap-5 w-full overflow-hidden justify-between max-[400px]:gap-3 max-[400px]:flex-col max-[400px]:items-center max-[400px]:justify-center lg:gap-8 lg:max-w-md">
-                  <div className="h-full w-16 flex-shrink-0 overflow-hidden max-[400px]:w-28 lg:w-24">
-                    <img
-                      src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-                      className="w-full h-full object-cover object-center"
-                    />
-                  </div>
-                  <div className="flex flex-col gap-4 overflow-hidden w-full">
-                    <h4 className="text-sm font-medium text-gray-700 truncate max-[400px]:text-xs lg:text-base">
-                      Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
-                    </h4>
-                    <div className="flex flex-col gap-4">
-                      <h3 className="text-base font-medium text-gray-800 max-[400px]:text-sm lg:text-lg">
-                        $109.95
-                      </h3>
-                      <div className="flex items-center gap-5 max-[400px]:gap-3">
-                        <button className="py-2">
-                          <TrashIcon className="w-5 h-5 text-gray-500 max-[400px]:w-4 max-[400px]:h-4" />
-                        </button>
-                        <div className="flex gap-4 max-[400px]:gap-2 items-center">
-                          <button className="bg-gray-700 w-8 h-8 px-2 py-2 flex items-center justify-center rounded-md max-[400px]:py-1">
-                            <MinusIcon className="w-4 h-4 text-white max-[400px]:w-3 max-[400px]:h-3" />
-                          </button>
-                          <span className="text-gray-800 text-base py-2 items-center font-medium max-[400px]:text-sm max-[400px]:py-1">
-                            1
-                          </span>
-                          <button className="bg-gray-700 w-8 h-8 px-2 py-2 flex items-center justify-center rounded-md max-[400px]:py-1">
-                            <PlusIcon className="w-4 h-4 text-white text-base max-[400px]:w-3 max-[400px]:h-3" />
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="flex gap-5 items-center px-2 py-4 max-[400px]:items-start lg:gap-10">
-                <div className="max-[400px]:mt-10 mt-0">
-                  <input
-                    type="checkbox"
-                    className="w-5 h-5 border-gray-500 max-[400px]:w-4 max-[400px]:h-4"
-                  />
-                </div>
-                <div className="flex gap-5 w-full overflow-hidden justify-between max-[400px]:gap-3 max-[400px]:flex-col max-[400px]:items-center max-[400px]:justify-center lg:gap-8 lg:max-w-md">
-                  <div className="h-full w-16 flex-shrink-0 overflow-hidden max-[400px]:w-28 lg:w-24">
-                    <img
-                      src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-                      className="w-full h-full object-cover object-center"
-                    />
-                  </div>
-                  <div className="flex flex-col gap-4 overflow-hidden w-full">
-                    <h4 className="text-sm font-medium text-gray-700 truncate max-[400px]:text-xs lg:text-base">
-                      Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
-                    </h4>
-                    <div className="flex flex-col gap-4">
-                      <h3 className="text-base font-medium text-gray-800 max-[400px]:text-sm lg:text-lg">
-                        $109.95
-                      </h3>
-                      <div className="flex items-center gap-5 max-[400px]:gap-3">
-                        <button className="py-2">
-                          <TrashIcon className="w-5 h-5 text-gray-500 max-[400px]:w-4 max-[400px]:h-4" />
-                        </button>
-                        <div className="flex gap-4 max-[400px]:gap-2 items-center">
-                          <button className="bg-gray-700 w-8 h-8 px-2 py-2 flex items-center justify-center rounded-md max-[400px]:py-1">
-                            <MinusIcon className="w-4 h-4 text-white max-[400px]:w-3 max-[400px]:h-3" />
-                          </button>
-                          <span className="text-gray-800 text-base py-2 items-center font-medium max-[400px]:text-sm max-[400px]:py-1">
-                            1
-                          </span>
-                          <button className="bg-gray-700 w-8 h-8 px-2 py-2 flex items-center justify-center rounded-md max-[400px]:py-1">
-                            <PlusIcon className="w-4 h-4 text-white text-base max-[400px]:w-3 max-[400px]:h-3" />
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              ))}
 
               {/* End product 1 */}
             </div>
