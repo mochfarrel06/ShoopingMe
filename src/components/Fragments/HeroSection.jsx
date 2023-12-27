be322e934796e19b70d5a26b207aee6ae82bbc4b
Moch Farrel <91719525+mochfarrel06@users.noreply.github.com>
2023-12-27 23:33:36 +0700

    Modification hero section with components

--- src/components/Fragments/HeroSection.jsx
+++ src/components/Fragments/HeroSection.jsx
@@ -1,42 +1,65 @@
 import Button from "../Elements/Button/Button";
 
-export default function HeroSection() {
+export default function HeroSection({children}) {
   return (
-    <div className="py-48 max-[400px]:py-36 md:py-56 lg:py-64">
-      <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-        <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-          Shop more easily, quickly, securely and best-selling transactions
-          here.{" "}
-          <a href="#" className="font-semibold text-blue-800">
-            <span className="absolute inset-0" aria-hidden="true" />
-            Shop now<span aria-hidden="true">&rarr;</span>
-          </a>
-        </div>
-      </div>
-      <div className="text-center flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-700 max-[400px]:text-3xl sm:text-5xl lg:text-6xl">
-          Shop for goods easily
-        </h1>
-        <p className="mt-6 text-lg leading-8 text-gray-500 max-[400px]:text-sm max-w-2xl">
-          Find the convenience of shopping for your favorite items quickly and
-          safely in our online shop. An easy, practical and satisfying shopping
-          experience only here!
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="#"
-            className="rounded-md bg-blue-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-900 max-[400px]:text-xs lg:px-6 lg:text-base"
-          >
-            Shop Now
-          </a>
-          <a
-            href="#"
-            className="text-sm font-semibold leading-6 text-gray-900 max-[400px]:text-xs lg:px-6 lg:text-base"
-          >
-            Learn more <span aria-hidden="true">→</span>
-          </a>
-        </div>
+    <div className="py-48 max-[400px]:py-36 md:py-56 lg:py-64">{children}</div>
+  );
+}
+
+function Header({btnTitle, description}) {
+  return (
+    <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+      <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+        {description}{" "}
+        <a href="#" className="font-semibold text-blue-800">
+          <span className="absolute inset-0" aria-hidden="true" />
+          {btnTitle}
+          <span aria-hidden="true">&rarr;</span>
+        </a>
       </div>
     </div>
   );
 }
+function Body({title, description}) {
+  return (
+    <div className="text-center flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold tracking-tight text-gray-700 max-[400px]:text-3xl sm:text-5xl lg:text-6xl">
+        {title}
+      </h1>
+      <p className="mt-6 text-lg leading-8 text-gray-500 max-[400px]:text-sm max-w-2xl">
+        {description}
+      </p>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="mt-10 flex items-center justify-center gap-x-6">
+      <Button
+        bgcolor={"bg-blue-800"}
+        txtcolor={"text-white"}
+        hoverBg={"hover:bg-blue-700"}
+        hoverTxt={"hover:text-white"}
+        padX={"px-4"}
+        padY={"py-2.5"}
+      >
+        Shop Now
+      </Button>
+      <Button
+        bgcolor={"bg-white"}
+        txtcolor={"text-gray-800"}
+        hoverBg={"hover:bg-gray-100"}
+        hoverTxt={"hover:text-gray-800"}
+        padX={"px-4"}
+        padY={"py-2.5"}
+      >
+        Learn more <span aria-hidden="true">→</span>
+      </Button>
+    </div>
+  );
+}
+
+HeroSection.Header = Header;
+HeroSection.Body = Body;
+HeroSection.Footer = Footer;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,7 +10,19 @@ export default function HomePage() {
     <>
       <Navbar />
       <MainLayouts>
-        <HeroSection />
+        <HeroSection>
+          <HeroSection.Header
+            btnTitle="Shop now"
+            description="Shop more easily, quickly, securely and best-selling transactions here."
+          />
+          <HeroSection.Body
+            title={"Shop for goods easily"}
+            description={
+              "Find the convenience of shopping for your favorite items quickly andsafely in our online shop. An easy, practical and satisfying shopping experience only here!"
+            }
+          />
+          <HeroSection.Footer />
+        </HeroSection>
         <ProductSection />
         <BannerSection />
       </MainLayouts>
