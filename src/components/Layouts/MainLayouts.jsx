6e478b2a6fd01d5260059dd322bd10c3f3575fa8
Moch Farrel <91719525+mochfarrel06@users.noreply.github.com>
2024-01-02 00:20:27 +0700

    Add load in mainlayouts

--- src/components/Layouts/MainLayouts.jsx
+++ src/components/Layouts/MainLayouts.jsx
@@ -1,9 +1,24 @@
+import {useEffect, useRef} from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import {useLocation} from "react-router-dom";
+
+const ScrollToTop = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!location.hash) {
+      window.scroll(0, 0);
+    }
+  }, [location]);
+
+  return null;
+};
 
 export default function MainLayouts({children}) {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <div className="px-6 py-14 lg:px-8 min-h-screen">
         <div className="mx-auto max-w-6xl">{children}</div>
