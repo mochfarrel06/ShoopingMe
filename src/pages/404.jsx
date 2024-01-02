447b116074940a019f150addd181e7087e830d93
Moch Farrel <91719525+mochfarrel06@users.noreply.github.com>
2024-01-02 17:13:32 +0700

    Moved ErorSection in folder

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,4 +1,4 @@
-import ErrorSection from "../components/Fragments/ErrorSection";
+import ErrorSection from "../components/Fragments/Error/ErrorSection";
 import MainLayouts from "../components/Layouts/MainLayouts";
 
 export default function ErrorPage() {
